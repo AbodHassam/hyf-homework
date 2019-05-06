cf75c0e1f4f53c67f06b45b45a00aba1c1c8213f
AbodHassam <43907626+AbodHassam@users.noreply.github.com>
2019-05-06 17:29:38 +0200

    updated

--- databases/week3/app.js
+++ databases/week3/app.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json());
 })); */
 
 let db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'hassamsaga',
-    database: 'hyf_db3',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     multipleStatements: true
     //port: 3000
 });
